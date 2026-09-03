281bff1410e4b35ff5bc094305bba7bd78ac6eff
Laurence Floriano Luciano <laurence.55925@alunosatc.edu.br>
2026-09-02 21:14:54 -0300

    Adicionar estrutura basica da arquitetura em camadas.

---
+++ Repositorio/Infra/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Repositorio.Infra
+{
+    internal class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options) 
+        { 
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Repositorio/Infra/GeradorDeServico.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Repositorio.Infra
+{
+    internal static class GeradorDeServico
+    {
+        public static ServiceProvider ServiceProvider;
+
+        public static DataContext CarregarContexto()
+        {
+            return ServiceProvider.GetService<DataContext>();
+        }
+    }
+}
