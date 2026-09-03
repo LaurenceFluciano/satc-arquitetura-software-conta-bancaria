1632c64a7bb59e105313f908942117a04f1805da
Laurence Floriano Luciano <laurence.55925@alunosatc.edu.br>
2026-09-02 21:38:15 -0300

    Adicionar configuracao do dominio e banco de dados.

---
+++ Dominio/Infra/ServiceExtensionDomain.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repositorio.Infra;
+
+namespace Dominio.Infra
+{
+    public static class ServiceExtensionDomain
+    {
+        public static void ConfigurarDominio(this IServiceCollection services, IConfiguration configuration)
+        {
+            ServiceExtensionRepository.ConfigurarRepositorio(services, configuration);
+        }
+    }
+}
---
+++ Repositorio/Infra/ServiceExtensionRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+
+
+namespace Repositorio.Infra
+{
+    public static class ServiceExtensionRepository
+    {
+        public static void ConfigurarRepositorio(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Sqlite");
+            services.AddDbContext<DataContext>(opt => opt.UseSqlite(connectionString));
+
+            GeradorDeServico.ServiceProvider = services.BuildServiceProvider();
+        }
+    }
+}
